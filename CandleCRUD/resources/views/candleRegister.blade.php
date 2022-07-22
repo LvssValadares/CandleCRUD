68cdad13bc24a575a9f3dcd7d1ae007894fa2338
Lucas Santos <lvss.valadares123@gmail.com>
2022-07-22 13:30:28 +0044

    First commit

---
+++ CandleCRUD/resources/views/candleRegister.blade.php
@@ -0,0 +1,2 @@
+<h1> Registre a vela desejada!!! </h1>
+<a href="/"> Voltar para página inicial</a>
---
+++ CandleCRUD/resources/views/candles.blade.php
@@ -0,0 +1,3 @@
+<h1> Lista de Velas</h1>
+<a href="/"> Voltar para página inicial</a>
+<a href="https://shopee.com.br/dimensionsartt" target="_blank"> Comprar </a>
